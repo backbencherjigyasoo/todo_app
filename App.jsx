ac181327ea05ac8bdbf854ec84acd84133bfc178
Gaurav <backbencherjigyasoo@gmail.com>
2024-07-24 22:02:16 +0530

    project started and running

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, useColorScheme, View} from 'react-native';
+
+// list of todo task
+// add button ==> input box and submit button
+// update list item
+// delete list item
+
+function App() {
+  return (
+    <View>
+      <Text>Hello, this is my first todo app</Text>
+    </View>
+  );
+}
+
+export default App;
